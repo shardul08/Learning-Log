55eddb88eead9c18ece37a316f6c7415ba29fe3b
shardul08 <sshardul007@gmail.com>
2018-03-05 00:44:39 +0530

    New topic addition for user

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Topic
+from .forms import TopicForm
 # Create your views here.
 
 def index(request):
@@ -20,3 +23,17 @@ def topic(request, topic_id):
 	context = {'topic':topic, 'entries':entries}
 	return render(request, 'learning_logs/topic.html', context)    
 	
+def new_topic(request):
+	"""Add a new topic."""
+	if request.method != 'POST':
+		# No data submitted, create new form
+		form = TopicForm()
+	else:
+		# POST data submitted, process data
+		form = TopicForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+	context = {'form': form}
+	return render(request, 'learning_logs/new_topic.html', context)
